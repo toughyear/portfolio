1054d2f659185f4b0c9b63bcc84bca9a2ad5e6b6
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2020-01-07 21:42:54 +0530

    First commit to site

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const text = document.querySelector(".main-text");
+
+var n = 0;
+let timer = setInterval(onTick, 2500);
+
+function onTick() {
+  if (n === 0) {
+    text.innerHTML = " Hi there! I'm <span >Rajeev</span>";
+
+    n = 1;
+    return;
+  }
+  if (n === 4) {
+    text.innerHTML = ' Hi there! I\'m <span class="jobs" ">Rajeev</span>';
+
+    n = 1;
+    return;
+  } else if (n == 1) {
+    text.innerHTML = ' Hi there! I\'m <span class="jobs">a Developer</span>';
+    n = 2;
+    return;
+  } else if (n == 2) {
+    text.innerHTML =
+      ' Hi there! I\'m <span class="jobs">a Motion Artist</span>';
+    n = 3;
+    return;
+  } else if (n == 3) {
+    text.innerHTML = ' Hi there! I\'m <span class="jobs">a Game Dev</span>';
+    n = 4;
+    return;
+  }
+}
+//to prevent autoscroll to bottom on refresh
+$(window).on("load", function() {
+  $(window).scrollTop(0);
+});
